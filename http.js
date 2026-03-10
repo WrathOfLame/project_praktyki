9726977317f658f42a762d8af4214a8ac1435cca
MAC <mac@MACs-MacBook-Pro.local>
2026-03-10 19:40:57 +0100

    Update server/html

--- http.js
+++ http.js
@@ -1,6 +1,32 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
 http.createServer((req, res) => {
-    res.write('Hello Worlds\n');
-    require('app.js');
-    res.end();
-}).listen(5000, 'localhost');
+    if(req.url === '/') {
+        const file = path.join(__dirname, 'index.html');
+        fs.readFile(file, (err, data)=>{
+            if(err){
+                res.statusCode = 500;
+                res.end('Server error')
+                return
+            }
+            res.setHeader('Content-Type', 'text/html');
+            res.end(data);
+        })
+    } else if (req.url === '/graph.js') {
+        const file = path.join(__dirname, 'graph.js');
+        fs.readFile(file, 'utf8', (err, data)=>{
+            if(err){
+                res.statusCode = 500;
+                res.end('Server error')
+                return
+            }
+            res.setHeader('Content-Type', 'application/javascript');
+            res.end(data);
+        })
+    } else {
+        res.statusCode = 404;
+        res.end('Page not found');
+    }
+}).listen(5000);
