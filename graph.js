af3b22872a778ba6fb3b5f9c856d05fa2f2e452b
Vladyslav Frolov <vladyslav.frolov@nexteer.com>
2026-03-20 12:17:04 +0100

    Adding improved look of project

--- graph.js
+++ graph.js
@@ -9,13 +9,19 @@ const maxHeight = ySize - margin.top - margin.bottom
 const xScale = d3.scaleLinear().range([0, maxWidth])
 const yScale = d3.scaleLinear().range([maxHeight, 0])
 
+// Adding colors to lines
+const colorScale = d3.scaleOrdinal(d3.schemeCategory10)
+
 // Set up svg container
-const svg = d3.select('#container').append('svg')
+const container = document.getElementById('container')
+const svg = d3.select(container).append('svg')
   .attr('width', xSize)
   .attr('height', ySize)
   .attr('style', 'cursor: crosshair;')
   .append('g')
   .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+  container.setAttribute('style', `width: ${xSize}px; height: ${ySize}px;`)
 
 // Axes generating
 const xAxis = d3.axisBottom(xScale)
@@ -29,43 +35,44 @@ const line = d3.line()
 // Load data and create graph
 let seriesData = []
 fetch('data_different_time_stamps.txt')
-  .then(res => res.json())
-  .then(raw => {
-    seriesData = Object.entries(raw).map(([key, points]) => ({
-      name: key,
-      values: points.map(([x, y]) => ({ x, y }))
-    }))
+    .then(res => res.json())
+    .then(raw => {
+        seriesData = Object.entries(raw).map(([key, points]) => ({
+            name: key,
+            values: points.map(([x, y]) => ({ x, y }))
+        }))
 
-const allXVals = seriesData.flatMap(s => s.values.map(v => v.x))
-const allYVals = seriesData.flatMap(s => s.values.map(v => v.y))
+        const allXVals = seriesData.flatMap(s => s.values.map(v => v.x))
+        const allYVals = seriesData.flatMap(s => s.values.map(v => v.y))
 
-xScale.domain(d3.extent(allXVals))
-yScale.domain([0, d3.max(allYVals)])
+        xScale.domain(d3.extent(allXVals))
+        yScale.domain([0, d3.max(allYVals)])
 
-svg.append('g')
-    .attr('transform', `translate(0, ${maxHeight})`)
-    .attr('class', 'x-axis')
-    .call(xAxis)
+        svg.append('g')
+            .attr('transform', `translate(0, ${maxHeight})`)
+            .attr('class', 'x-axis')
+            .call(xAxis)
 
-svg.append('g')
-    .attr('class', 'y-axis')
-    .call(yAxis)
+        svg.append('g')
+            .attr('class', 'y-axis')
+            .call(yAxis)
 
-svg.selectAll('path.series')
-    .data(seriesData)
-    .join('path')
-    .attr('class', 'series')
-    .attr('fill', 'none')
-    .attr('stroke', 'steelblue')
-    .attr('stroke-width', 1.5)
-    .attr('d', d => line(d.values))
+        svg.selectAll('path.series')
+            .data(seriesData)
+            .join('path')
+            .attr('class', 'series')
+            .attr('fill', 'none')
+            .attr('stroke', d => colorScale(d.name))
+            .attr('stroke-width', 1.5)
+            .attr('d', d => line(d.values))
+        
+        // creating additional lines for each point in graph
+        
 })
 .catch(err => console.error('Error loading data:', err))
 
 // Creating cursor tracing lines
 const cursor_field = document.getElementById('cursor_locator')
-const container = document.getElementById('container')
-
 const vertical_line = svg.append('line')
     .attr('stroke', 'gray')
     .attr('stroke-dasharray', 5,5)
@@ -145,25 +152,18 @@ function brushed(event){
     if (!event.selection) return
     zoomed = true
     // Adding dots for each point in graph
-    fetch('data_different_time_stamps.txt').then(res => res.json())
-        .then(raw => {
-            seriesData = Object.entries(raw).map(([key, points]) => ({
-                name: key,
-                values: points.map(([x, y]) => ({ x, y }))
-            }))
-            seriesData.forEach(s => {
-                svg.selectAll(`circle.point-${s.name}`)
-                    .data(s.values)
-                    .join('circle')
-                    .attr('class', `point-${s.name}`)
-                    .attr('cx', d => xScale(d.x))
-                    .attr('cy', d => yScale(d.y))
-                    .attr('r', 3)
-                    .attr('fill', 'white')
-                    .attr('stroke', 'steelblue')
+    seriesData.forEach(s => {
+        svg.selectAll(`circle.point-${s.name}`)
+            .data(s.values)
+            .join('circle')
+            .attr('class', `point-${s.name}`)
+            .attr('cx', d => xScale(d.x))
+            .attr('cy', d => yScale(d.y))
+            .attr('r', 3)
+            .attr('fill', 'white')
+            .attr('stroke', 'steelblue')
 
-            })
-        })
+    })
     
     //brushing
     const x0 = event.selection[0][0]
@@ -173,12 +173,18 @@ function brushed(event){
 
     xScale.domain(newXDomain)
 
-    svg.select('.x-axis').call(xAxis)
+    svg.select('.x-axis')
+        .attr("transform", `translate(0, ${maxHeight})`)
+        .call(xAxis)
+    
+    svg.select('.y-axis')
+        .attr("transform", `translate(0,0)`)
+        .call(yAxis)
 
-    const line = d3.line()
+    const zoomedLine = d3.line()
         .x(d => xScale(d.x))
         .y(d => yScale(d.y))
-    svg.selectAll('.series').attr('d', d => line(d.values))
+    svg.selectAll('.series').attr('d', d => zoomedLine(d.values))
     svg.select(".brush").call(brush.move, null)
 }
 
@@ -208,7 +214,7 @@ fetch('data_different_time_stamps.txt')
       name: key,
       values: points.map(([x, y]) => ({ x, y }))
     }))
-    const counters = {}
+    const counters = {x: [], y: []}
     seriesData.forEach(s => {
         const row = document.createElement('tr')
         legend.appendChild(row)
@@ -224,30 +230,39 @@ fetch('data_different_time_stamps.txt')
         legendName.setAttribute('id', `legend-${s.name}`)
         row.appendChild(legendName)
         
-        // Adding mouse move event to show y value at current x for each graph
-        const counter = document.createElement('span')
-        row.appendChild(counter)
-        counters[s.name] = counter
-        
+        // Adding mouse move event to show x and y values in legend
+        const counterY = document.createElement('span')
+        const counterX = document.createElement('span')
+        row.appendChild(counterX)
+        row.appendChild(counterY)
+        counters.y.push(counterY)
+        counters.x.push(counterX)
+
         container.addEventListener('mousemove', event => {
             const rect = container.getBoundingClientRect()
             const mouseX = event.clientX - rect.left - margin.left;
-
-            const xVal = xScale.invert(mouseX);
-            const values = s.values;
+            const mouseY = event.clientY - rect.top - margin.top;
+            const xVal = xScale.invert(mouseX)
+            const yVal = yScale.invert(mouseY)
+            const values = s.values
+            let closest = s.values[0]
+            values.forEach(p => {
+                if (Math.abs(p.x - xVal) < Math.abs(closest.x - xVal)) {
+                    closest = p;
+                }
+            })
             for (let i = 0; i < values.length - 1; i++) {
-                const p1 = values[i];
-                const p2 = values[i + 1];
-            if (
-                (xVal >= p1.x && xVal <= p2.x) ||
-                (xVal >= p2.x && xVal <= p1.x)
+                const p1 = values[i]
+                const p2 = values[i + 1]
+                if (
+                    (xVal >= p1.x && xVal <= p2.x) || 
+                    (yVal >= p1.y && yVal <= p2.y)
                 ){
-                    const t = (xVal - p1.x) / (p2.x - p1.x);
-                    const y = p1.y + t * (p2.y - p1.y);
-
-                    counter.textContent = ` = ${y.toFixed(2)}`;
-                    row.appendChild(counter)
-                    
+                    const t = (xVal - p1.x) / (p2.x - p1.x)
+                    const y = p1.y + t * (p2.y - p1.y)
+                    counterX.textContent = ` x = ${closest.x.toFixed(3)}`;
+                    counterY.textContent = ` y = ${y.toFixed(2)}`;
+                    row.appendChild(counterY)
                     break;
                 }
             }
@@ -258,17 +273,17 @@ fetch('data_different_time_stamps.txt')
         checkbox.addEventListener('change', () => {
             const line = svg.selectAll('.series').filter(d => d.name === s.name)
             if(checkbox.checked){
-                line.style('display', null)
-                legendName.style.textDecoration = null
-                svg.selectAll(`circle.point-${s.name}`)
-                    .data(s.values)
-                    .join('circle')
-                    .attr('class', `point-${s.name}`)
-                    .attr('cx', d => xScale(d.x))
-                    .attr('cy', d => yScale(d.y))
-                    .attr('r', 3)
-                    .attr('fill', 'white')
-                    .attr('stroke', 'steelblue')
+            line.style('display', null)
+            legendName.style.textDecoration = null
+            svg.selectAll(`circle.point-${s.name}`)
+                .data(s.values)
+                .join('circle')
+                .attr('class', `point-${s.name}`)
+                .attr('cx', d => xScale(d.x))
+                .attr('cy', d => yScale(d.y))
+                .attr('r', 3)
+                .attr('fill', 'white')
+                .attr('stroke', 'steelblue')
             } else {
                 line.style('display', 'none')
                 legendName.style.textDecoration = 'line-through'
@@ -296,21 +311,21 @@ container.addEventListener('mousemove', event => {
             });
 
             if(document.getElementById(`checkbox-${s.name}`).checked) {
-            // Reset all points first
-            svg.selectAll(`circle.point-${s.name}`)
-                .data(s.values)
-                .join('circle')
-                .attr('class', `point-${s.name}`)
-                .attr('cx', d => xScale(d.x))
-                .attr('cy', d => yScale(d.y))
-                .attr('r', 3)
-                .attr('fill', 'white')
-                .attr('stroke', 'steelblue')
+                // Reset all points first
+                svg.selectAll(`circle.point-${s.name}`)
+                    .data(s.values)
+                    .join('circle')
+                    .attr('class', `point-${s.name}`)
+                    .attr('cx', d => xScale(d.x))
+                    .attr('cy', d => yScale(d.y))
+                    .attr('r', 3)
+                    .attr('fill', 'white')
+                    .attr('stroke', 'steelblue')
 
-            // Highlight only the closest one
-            svg.selectAll(`circle.point-${s.name}`)
-                .filter(d => d === closest)
-                .attr("fill", "gray");
+                // Highlight only the closest one
+                svg.selectAll(`circle.point-${s.name}`)
+                    .filter(d => d === closest)
+                    .attr("fill", "gray");
             }
         });
     }
